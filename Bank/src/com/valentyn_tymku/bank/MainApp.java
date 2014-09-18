80aff41e7ddd6c0153ed81b2c2f457be5a0e5320
vtymktc <vtymktc@chclass15.training.local>
2014-09-18 18:36:08 +0300

    88888

--- Bank/src/com/valentyn_tymku/bank/MainApp.java
+++ Bank/src/com/valentyn_tymku/bank/MainApp.java
@@ -1,9 +1,13 @@
 package com.valentyn_tymku.bank;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,8 +17,8 @@ import javax.xml.bind.Marshaller;
 
 public class MainApp {
 	public Customer dataCustomer() {
-		Customer cust1 = new Customer("James", "Hetfield", "43, SomeStreet st., Ukraine",
-				"+80992233444", "jh@gmail.com");
+		Customer cust1 = new Customer("James", "Hetfield",
+				"43, SomeStreet st., Ukraine", "+80992233444", "jh@gmail.com");
 		return cust1;
 
 	}
@@ -22,7 +26,8 @@ public class MainApp {
 	public Account dataAccount() {
 		Customer cust1 = dataCustomer();
 
-		Account acc1 = new Account(951798684, "acc1", Currencies.USD, 100, 10, cust1);
+		Account acc1 = new Account(951798684, "acc1", Currencies.USD, 100, 10,
+				cust1);
 		return acc1;
 
 	}
@@ -49,9 +54,9 @@ public class MainApp {
 
 	public void marshallExample() {
 		try {
-			File fileCustomers = new File( "customers.xml");
-			File fileAccounts = new File( "accounts.xml");
-			
+			File fileCustomers = new File("customers.xml");
+			File fileAccounts = new File("accounts.xml");
+
 			JAXBContext context = JAXBContext.newInstance(Customer.class);
 			Marshaller marshaller = context.createMarshaller();
 			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,
@@ -71,58 +76,112 @@ public class MainApp {
 					"marshallExample threw JAXBException", exception);
 		}
 	}
-	
-	
+
 	public void unmarshallExample() {
 
 	}
 	
-	public void out(Dst dst) throws IOException{
+	public void in(Dst dst) throws IOException {
+		switch (dst) {
+		case TXT:
+			BufferedReader cw = new BufferedReader(new FileReader(
+					"customers.txt"));
+			BufferedReader ca = new BufferedReader(new FileReader(
+					"accounts.txt"));
+			
 		
-		switch (dst){
-		case TXT : 
-			BufferedWriter cw = new BufferedWriter(new FileWriter("customers.txt"));
-			BufferedWriter ca = new BufferedWriter(new FileWriter("accounts.txt"));
-			
-			cw.write(dataCustomer().toString());
-			ca.write(dataAccount().toString());
-			
-			cw.close();
-			ca.close();
-			break;
-		case SOUT: 
+
+			while (true){
+				String str = cw.readLine();
+				if (str.length()<=0)break;
+				ArrayList<String> arr = new ArrayList<String>(); 
+					arr.addAll(str.split("|"));
+				
+				
+			}
+
+		case SOUT:
 			System.out.println(dataCustomer());
 			System.out.println(dataAccount());
 			break;
-		case XML: 
+		case XML:
 			MainApp instance = new MainApp();
 			instance.marshallExample();
 			break;
 		}
-		
-		
-		
 	}
 
-	public static void main(String[] args) {
+	public void out(Dst dst) throws IOException {
+
+		switch (dst) {
+		case TXT:
+			BufferedWriter cw = new BufferedWriter(new FileWriter(
+					"customers.txt"));
+			BufferedWriter ca = new BufferedWriter(new FileWriter(
+					"accounts.txt"));
+
+			cw.write(dataCustomer().toString());
+			ca.write(dataAccount().toString());
+
+			cw.close();
+			ca.close();
+			break;
+		case SOUT:
+			System.out.println(dataCustomer());
+			System.out.println(dataAccount());
+			break;
+		case XML:
+			MainApp instance = new MainApp();
+			instance.marshallExample();
+			break;
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
 
 		MainApp instance = new MainApp();
-		try {
+	/*	try {
 			instance.out(Dst.TXT);
 			instance.out(Dst.SOUT);
 			instance.out(Dst.XML);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}*/
+		
+		System.out.println("What is input destination?");
+		System.out.println("1. Console.");
+		System.out.println("2. Text file.");
+		System.out.println("3. XML file.");
+		System.out.println("Choose one: ");
+		
+		BufferedReader r1 = new BufferedReader(new InputStreamReader(System.in));
+		String tIn = r1.readLine();
+		
+		switch (Integer.valueOf(tIn)){
+		case 1: instance.in(Dst.SOUT); break;
+		case 2: instance.in(Dst.TXT); break;
+		case 3: instance.in(Dst.XML); break;
 		}
 		
 		
+		System.out.println("What is output destination?");
+		System.out.println("1. Console.");
+		System.out.println("2. Text file.");
+		System.out.println("3. XML file.");
+		System.out.println("Choose one: ");
 		
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		String tOut = r.readLine();
 		
+		switch (Integer.valueOf(tOut)){
+		case 1: instance.out(Dst.SOUT); break;
+		case 2: instance.out(Dst.TXT); break;
+		case 3: instance.out(Dst.XML); break;
+		}
 		
-		
-		
-		
+
 		// instance.unmarshallExample();
 
 		/*
